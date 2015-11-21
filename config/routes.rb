5d0953ebaabc8ce7104b13549a412113facbfe6f
yuxing <yuxing0129@gmail.com>
2015-11-21 23:17:45 +0900

    Added a Contact page (solves exercise 3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about' => 'static_pages#about', as: 'about'
 
+  get 'static_pages/contact' => 'static_pages#contact', as: 'contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
