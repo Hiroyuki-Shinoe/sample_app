c46d69944f82329e9429e52b7a4451ff01e9e7f0
yuxing <yuxing0129@gmail.com>
2015-11-22 00:48:36 +0900

    Added advanced setting for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# minitest-reporter gem 使用のための記述
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
