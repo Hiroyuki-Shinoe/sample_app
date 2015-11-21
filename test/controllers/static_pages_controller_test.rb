c5b0941f7ef3e562c8d775f20b6145497d552e96
yuxing <yuxing0129@gmail.com>
2015-11-21 21:13:56 +0900

    Refactored a little

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,17 +3,23 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
+    # client側のrequestに対して、 serverからresponseが返ってくるかを検証
     assert_response :success
+    # HTMLファイルに titleタグがあり、そのタグに囲まれているのが、
+    # "Home | Ruby on Rails Tutorial Sample App"であるかを検証。
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
